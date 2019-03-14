f66c3eaccb5bb5de3130d3ed9ea2e4d1b3e49fef
KajG <kaaiman99@hotmail.com>
2019-03-14 08:55:28 +0100

    small change

--- SmikkelSmakkelEendjes/Assets/features/food/scripts/FoodSpawner.cs
+++ SmikkelSmakkelEendjes/Assets/features/food/scripts/FoodSpawner.cs
@@ -9,6 +9,8 @@ public class FoodSpawner : Singleton<FoodSpawner> {
     private const int CHANCE_THRESHOLD = 20; //Percent chance of special food
     private const float MAX_FOOD_SPEED = 0.04f;
     private const float MIN_FOOD_SPEED = 0.01f;
+    private const float MAX_ROTATION_SPEED = 10;
+    private const float MIN_ROTATION_SPEED = -10;
     private const int SPAWN_RATE = 1;
     private string[] _foodNames = new string[]{"erwt1", "erwt2", "erwt3"};
 
@@ -21,18 +23,29 @@ public class FoodSpawner : Singleton<FoodSpawner> {
         {
             GameObject obj = Instantiate(foodObj, new Vector3(3, i), Quaternion.identity);
             Food tempFood = (Food)obj.AddComponent(typeof(Food));
-            float randomSpeed = UnityEngine.Random.Range(MIN_FOOD_SPEED, MAX_FOOD_SPEED);
-            int randomSpriteIndex = UnityEngine.Random.Range(0, _foodNames.Length);
-            tempFood.Init(i, randomSpeed, randomSpeed * 500, 1, _foodNames[randomSpriteIndex]);
+            tempFood.Init(i, RandomSpeed(), RandomRotation(), 1, _foodNames[RandomSpriteIndex()]);
             _foods[i] = tempFood;
         }
         FoodController.Instance.SaveFood(_foods);
     }
     public void MoveBackFood(int id, float spawnPosX)
     {
-        int randomSpriteIndex = UnityEngine.Random.Range(0, _foodNames.Length);
-        float randomSpeed = UnityEngine.Random.Range(MIN_FOOD_SPEED, MAX_FOOD_SPEED);
-        FoodController.Instance.InitFood(id, randomSpeed, randomSpeed * 500, 1, _foodNames[randomSpriteIndex]);
+        FoodController.Instance.InitFood(id, RandomSpeed(), RandomRotation(), 1, _foodNames[RandomSpriteIndex()]);
         FoodController.Instance.MoveBackFood(id, spawnPosX);
+    }
+    public float RandomSpeed()
+    {
+        float randomSpeed = UnityEngine.Random.Range(MIN_FOOD_SPEED, MAX_FOOD_SPEED);
+        return randomSpeed;
+    }
+    public float RandomRotation()
+    {
+        float randomSpeed = UnityEngine.Random.Range(MIN_ROTATION_SPEED, MAX_ROTATION_SPEED);
+        return randomSpeed;
+    }
+    public int RandomSpriteIndex()
+    {
+        int randomSpriteIndex = UnityEngine.Random.Range(0, _foodNames.Length);
+        return randomSpriteIndex;
     }
 }
